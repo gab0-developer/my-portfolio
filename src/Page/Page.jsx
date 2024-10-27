4d2a3ad30fb1e60916f107a618581f4f5c2e8e8c
gabrielDev <gabrielrpdev2@gmail.com>
2024-10-27 13:23:05 -0400

    add footer

--- src/Page/Page.jsx
+++ src/Page/Page.jsx
@@ -5,6 +5,7 @@ import Perfil from './perfil/Perfil';
 import Proyectos from './proyectos/Proyectos';
 import Contacto from './contacto/Contacto';
 import Navbard from '../components/Navbard';
+import Footer from '../components/footer';
 
 function Page() {
   return (
@@ -14,6 +15,7 @@ function Page() {
       <div id="perfil"><Perfil /></div>
       <div id="proyectos"><Proyectos /></div>
       <div id="contactos"><Contacto /></div>
+      <div id="contactos"><Footer /></div>
       {/* <Inicio />
       <Perfil />
       <Proyectos />
