f13d78d3114cdbd3d7ced91e93ff98ebd905c0ee
gabrielDev <gabrielrpdev2@gmail.com>
2024-10-27 12:56:31 -0400

    add Navbard

--- src/Page/Page.jsx
+++ src/Page/Page.jsx
@@ -4,14 +4,20 @@ import Inicio from "./inicio/Inicio";
 import Perfil from './perfil/Perfil';
 import Proyectos from './proyectos/Proyectos';
 import Contacto from './contacto/Contacto';
+import Navbard from '../components/Navbard';
 
 function Page() {
   return (
     <>
-      <Inicio />
+      <Navbard />
+      <div id="inicio"><Inicio /></div>
+      <div id="perfil"><Perfil /></div>
+      <div id="proyectos"><Proyectos /></div>
+      <div id="contactos"><Contacto /></div>
+      {/* <Inicio />
       <Perfil />
       <Proyectos />
-      <Contacto />
+      <Contacto /> */}
     </>
   )
 }
---
+++ src/components/Navbard.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import { Button, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+
+
+function Navbard() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [activeButton, setActiveButton] = useState(""); // Estado para el botón activo
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    // Función para actualizar el botón activo
+    const handleButtonClick = (namebutton) => {
+        setActiveButton(namebutton);
+    };
+
+    return (
+        <>
+            <nav className="navbar">
+                <ul className={`navbar-links ${isOpen ? "open" : ""}`}>
+                    <li>
+                        <a href="#inicio">
+                            <Button
+                                variant="outlined"
+                                startIcon={
+                                    <HomeIcon color={activeButton === "inicio" ? "primary" : "inherit"} />
+                                }
+                                onClick={() => handleButtonClick("inicio")}
+                                color={activeButton === "inicio" ? "primary" : "inherit"}
+                            >
+                                Inicio
+                            </Button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#perfil">
+                            <Button
+                                variant="outlined"
+                                startIcon={
+                                    <PersonPinIcon color={activeButton === "perfil" ? "primary" : "inherit"} />
+                                }
+                                onClick={() => handleButtonClick("perfil")}
+                                color={activeButton === "perfil" ? "primary" : "inherit"}
+                            >
+                                Perfil
+                            </Button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#proyectos">
+                            <Button
+                                variant="outlined"
+                                startIcon={
+                                    <MiscellaneousServicesIcon color={activeButton === "proyectos" ? "primary" : "inherit"} />
+                                }
+                                onClick={() => handleButtonClick("proyectos")}
+                                color={activeButton === "proyectos" ? "primary" : "inherit"}
+                            >
+                                Proyectos
+                            </Button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contactos">
+                            <Button
+                                variant="outlined"
+                                startIcon={
+                                    <ContactMailIcon color={activeButton === "contactos" ? "primary" : "inherit"} />
+                                }
+                                onClick={() => handleButtonClick("contactos")}
+                                color={activeButton === "contactos" ? "primary" : "inherit"}
+                            >
+                                Contacto
+                            </Button>
+                        </a>
+                    </li>
+                </ul>
+                <div className="navbar-toggle">
+                    <IconButton aria-label="menu" onClick={toggleMenu}>
+                        <MenuIcon />
+                    </IconButton>
+                </div>
+            </nav>
+        </>
+    );
+
+}
+
+export default Navbard
