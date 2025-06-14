2c510ffec066b72e3e9c03d90932049fa1058101
gabrielDev <gabrielrpdev2@gmail.com>
2025-06-14 18:37:32 -0400

    feat: Actualizado inicio del portafolio

--- src/Page/inicio/Inicio.jsx
+++ src/Page/inicio/Inicio.jsx
@@ -7,90 +7,58 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 
 import './Inicio.css'
 import img from "../../assets/img/Exportar_img_all";
+import TextAnimation from '../../components/TextAnimation';
 
-// const textgradient ={
-//   backgroundImage: 'linear-gradient(to right, #00DFA2, #0079FF)', // Degradado de color
-//   WebkitBackgroundClip: 'text', // Establece el área del texto afectada por el fondo
-//   color: 'transparent', // Hace el texto transparente para mostrar el degradado de fondo
-  
-// }
+
 function Inicio() {
 
-
+  
   return (
     <>
       <Container id='inicio' maxWidth="xl" className='container-padre'>
         <Box component='div' className='section container-inicio' sx={{ minHeight: '100vh', scrollMarginTop: '64px' }}>
           <Box component='div' className='container-title-inicio'>
+            {/* animatejs */}
             <Box component='div' className='title-inicio'>
-              <Typography variant="h4" className='text-perfil' id='text-titles-subtitles' data-aos="fade-up" >Developer</Typography>
+              <Typography variant="h4" className='text-perfil' id='text-titles-subtitles' data-aos="fade-up" >Gabriel Rincón Developer</Typography>
+              <Typography variant="h6" className='text-perfil text-titles-description' data-aos="fade-up" >🌐Conectar a través del Software🌐</Typography>
+              
+                <TextAnimation/>
             </Box>
             <Box component='div'>
-              <Typography variant="h2" className='textanimation-perfil' ></Typography>
-            </Box>
-            <Box component='div' className='link-perfil' data-aos="fade-up">
-                <Tooltip title="Descarga mi currículum para descubrir cómo mis habilidades y experiencia pueden desarrolar tus proyectos web." placement="top" arrow>
-                  <a href="https://gab0-developer.github.io/my-portfolio/cv/CV_GABRIEL_RINCON_2025.pdf"
-                    download="CV_GABRIEL_RINCON_2025.pdf"
-                    target="_blank" rel="noopener noreferrer"
-                    style={{color:'#fff',marginBottom:'2px'}}>
-                    <Fab variant="extended" color='success' sx={{mr:'0.5rem'}}>
-                      <CloudDownloadIcon sx={{ mr: 1 }} color='' />
-                        Currículum
-                    </Fab>
-                  </a>
-                </Tooltip>
-                <Tooltip title="Hola!, si quieres saber más de mí perfil profesional y experiencias, visita mi LinkedIn." placement="top" arrow>
-                  <a href="https://www.linkedin.com/in/gabriel-rinconp" target="_blank" rel="noopener noreferrer">
-                    <Fab color='primary'>
-                      <LinkedInIcon />
-                    </Fab>
-                  </a>
-                </Tooltip>
-                <Tooltip title="Ey! Podrás ver el codigo fuente de mis proyectos personales y perfil como Web developer." placement="top" arrow>
+              <Box component='div' className='link-perfil' data-aos="fade-up">
+                  <Tooltip title="Descarga mi currículum para descubrir cómo mis habilidades y experiencia pueden desarrolar tus proyectos web." placement="top" arrow>
+                    <a href="https://gab0-developer.github.io/my-portfolio/cv/CV_GABRIEL_RINCON_2025.pdf"
+                      download="CV_GABRIEL_RINCON_2025.pdf"
+                      target="_blank" rel="noopener noreferrer"
+                      style={{color:'#fff',marginBottom:'2px'}}>
+                      <Fab variant="extended" color='success' sx={{mr:'0.5rem'}}>
+                        <CloudDownloadIcon sx={{ mr: 1 }} color='' />
+                          Currículum
+                      </Fab>
+                    </a>
+                  </Tooltip>
+                  <Tooltip title="Hola!, si quieres saber más de mí perfil profesional y experiencias, visita mi LinkedIn." placement="top" arrow>
+                    <a href="https://www.linkedin.com/in/gabriel-rinconp" target="_blank" rel="noopener noreferrer">
+                      <Fab color='primary'>
+                        <LinkedInIcon />
+                      </Fab>
+                    </a>
+                  </Tooltip>
+                  <Tooltip title="Ey! Podrás ver el codigo fuente de mis proyectos personales y perfil como Web developer." placement="top" arrow>
 
-                  <a href="https://github.com/gab0-developer" target="_blank" rel="noopener noreferrer" style={{margin:'0 0.5rem'}}>
-                    <Fab color='default'>
-                      <GitHubIcon />
-                    </Fab>
-                  </a>
-                </Tooltip>
+                    <a href="https://github.com/gab0-developer" target="_blank" rel="noopener noreferrer" style={{margin:'0 0.5rem'}}>
+                      <Fab color='default'>
+                        <GitHubIcon />
+                      </Fab>
+                    </a>
+                  </Tooltip>
               </Box>
+            
+            </Box>
           </Box>
-          {/* image perfil */}
-          <Box component='div' className='foto-perfil' data-aos="fade-left">
-            <img src={img.perfil_portfolio} alt="" style={{width:'100%'}} />
-          </Box>
-          
-          {/* burbujas */}
-          {/* <Box component='div' className='container-burbujas'>
-            <Box component='div' className='burbujas'>
-              <img src={img.Laravel_Logo} alt="" style={{width:'40px'}} />
-            </Box>
-            <Box component='div' className='burbujas'>
-              <img src={img.logo_reactjs} alt="" style={{width:'20px'}} />
-            </Box>
-            <Box component='div' className='burbujas'>
-              <img src={img.logo_MaterilUI} alt="" style={{width:'20px'}} />
-            </Box>
-            <Box component='div' className='burbujas'>
-              <img src={img.Bootstrap_logo} alt="" style={{width:'20px'}} />
-            </Box>
-            <Box component='div' className='burbujas'>
-              <img src={img.oracle_logo} alt="" style={{width:'20px'}} />
-            </Box>
-            <Box component='div' className='burbujas'>
-              <img src={img.mysql_logo} alt="" style={{width:'15px'}} />
-            </Box>
-            <Box component='div' className='burbujas'>
-              <img src={img.postgre_logo} alt="" style={{width:'20px'}} />
-            </Box>
-            <Box component='div' className='burbujas'>
-              <img src={img.log_vc_code} alt="" style={{width:'15px'}} />
-            </Box>
-          </Box> */}
         </Box>
-        
+
       </Container>
     </>
   )
---
+++ src/components/TextAnimation.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect } from "react";
+import anime from "animejs"; // Usa animejs@2.0.2
+import "./AnimatedText.css";
+import { Box } from "@mui/material";
+
+const TextAnimation = () => {
+  useEffect(() => {
+    const ml4 = {
+      opacityIn: [0, 1],
+      scaleIn: [0.2, 1],
+      scaleOut: 3,
+      durationIn: 800,
+      durationOut: 600,
+      delay: 500,
+    };
+
+    anime
+      .timeline({ loop: true })
+      .add({
+        targets: ".ml4 .letters-1",
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn,
+      })
+      .add({
+        targets: ".ml4 .letters-1",
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay,
+      })
+      .add({
+        targets: ".ml4 .letters-2",
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn,
+      })
+      .add({
+        targets: ".ml4 .letters-2",
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay,
+      })
+      .add({
+        targets: ".ml4 .letters-3",
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn,
+      })
+      .add({
+        targets: ".ml4 .letters-3",
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay,
+      })
+      .add({
+        targets: ".ml4",
+        opacity: 0,
+        duration: 500,
+        delay: 500,
+      });
+  }, []);
+
+  return (
+    <Box component='center'>
+      <h1 className="ml4">
+        <span className="letters letters-1">Full-Stack!</span>
+        <span className="letters letters-2">Innovator!</span>
+        <span className="letters letters-3">Problem Solver!</span>
+      </h1>
+    </Box>
+  );
+};
+
+export default TextAnimation;
