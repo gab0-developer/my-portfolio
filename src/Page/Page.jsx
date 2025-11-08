9794ac21fc6b735855cc7b4ebb5b9614726f3b76
gabrielDev <gabrielrpdev2@gmail.com>
2025-11-08 18:38:48 -0400

    refactor:  corregir la ruta de importacion de proyectos personales

--- src/Page/Page.jsx
+++ src/Page/Page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Inicio from "./inicio/Inicio";
 import Perfil from './perfil/Perfil';
-import Proyectos from './proyectos/Proyectos';
+import Proyectos from './proyectos/proyectos_personales/Proyectos';
 import ProyectoLaboral from './proyectos/proyectos_laborales/ProyectoLaboral';
 import Contacto from './contacto/Contacto';
 import Footer from '../components/Footer/Footer';
